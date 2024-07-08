69c047cf8e45096413ec68370fbb4ae6cd937fe8
dvader <spranay07@gmail.com>
2024-07-07 21:13:45 -0400

    WIP: [#] Bit Set, Clear and Toggle using bit wise operators

---
+++ set_clearNBitPosition.cpp
@@ -0,0 +1,46 @@
+/* Set, clear and toggle  the nth bit in a number*/
+#include "stdio.h"
+#include <iostream>
+#include "string.h"
+#include "math.h"
+#include <algorithm>
+
+using namespace std;
+
+int setNthBit(int userinput, int position)
+{
+    return (userinput|(1<<position));
+}
+
+int clearNthBit(int userinput, int position)
+{
+    return (userinput &(~(1<<position)));
+}
+
+int toggleNthBit(int userinput, int position)
+{
+    return (userinput^(1<<position));
+}
+
+int main(void){
+    int userInput, positionClear, positionSet, positionToggle;
+    cout << "input a number " << endl;
+    cin >> userInput;
+    
+    cout << "input postion to clear" << endl;
+    cin >> positionClear;
+    
+    cout << "input postion to Set" << endl;
+    cin >> positionSet;
+
+    cout << "input postion to Toggle" << endl;
+    cin >> positionToggle;
+
+    int val1 = clearNthBit(userInput, positionClear);
+    int val2 = setNthBit(userInput, positionSet);
+    int val3 = toggleNthBit(userInput, positionToggle);
+
+    cout << "The " << userInput << " after clear of " << positionClear << " is " << val1 << endl;
+    cout << "The " << userInput << " after Set of " << positionSet << " is " << val2 << endl;
+    cout << "The " << userInput << " after Toggle of " << positionSet << " is " << val3 << endl;
+}
