02a5a490feee8786bc8b86f5d258d365522e5ae6
dvader <spranay07@gmail.com>
2024-07-07 21:25:16 -0400

    WIP: [#] Remove the right most set bit using bitwise function added.

--- set_clearNBitPosition.cpp
+++ set_clearNBitPosition.cpp
@@ -22,6 +22,11 @@ int toggleNthBit(int userinput, int position)
     return (userinput^(1<<position));
 }
 
+int removeRightmostSetbit(int userinput)
+{
+    return (userinput &(userinput - 1));
+}
+
 int main(void){
     int userInput, positionClear, positionSet, positionToggle;
     cout << "input a number " << endl;
@@ -39,8 +44,10 @@ int main(void){
     int val1 = clearNthBit(userInput, positionClear);
     int val2 = setNthBit(userInput, positionSet);
     int val3 = toggleNthBit(userInput, positionToggle);
+    int val4 = removeRightmostSetbit(userInput);
 
     cout << "The " << userInput << " after clear of " << positionClear << " is " << val1 << endl;
     cout << "The " << userInput << " after Set of " << positionSet << " is " << val2 << endl;
     cout << "The " << userInput << " after Toggle of " << positionSet << " is " << val3 << endl;
+    cout << "The " << userInput << " after removal of rightmost Set Bit is " << val4 << endl;
 }
